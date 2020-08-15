2009c84dff56d83757c127ecca6afc956f704389
karunpant-pcln <karun.pant@priceline.com>
2020-08-16 02:25:57 +0530

    Start button adjustments.

--- YogaExplore/StartViewController.swift
+++ YogaExplore/StartViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class StartViewController: UIViewController, NavigationBarCustomizable {
 
-    @IBOutlet weak var btnStart: UIButton! 
+    @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        btnStart.roundedCapsule()
+        roundAndDropShadow()
         transparentNavigationBar()
         addCustomBackButton(UIImage(named: "back"), tapHandler: UITapGestureRecognizer(target: self, action: #selector(handleBackTap)))
+    }
+    private func roundAndDropShadow() {
+        let radius = startButton.frame.height/2
+        startButton.layer.cornerRadius = radius
+        startButton.layer.shadowColor = Colors.componentBackground.color.cgColor
+        startButton.layer.shadowOffset = CGSize(width: 0, height: 10)
+        startButton.layer.shadowOpacity = 0.5
+        startButton.layer.shadowRadius = 10
+        
     }
     @objc private func handleBackTap() {
         navigationController?.popViewController(animated: true)
